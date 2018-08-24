7b7d298b7ccb47b937f1f37af3fcff2dd3a93fa0
wuyingqiang <794870409@qq.com>
2018-08-25 02:12:54 +0800

    [UPDATE] change test into spring-test 1. use Spring-Test 2. add maven dependency

--- src/test/java/com/unclew/web/HomeControllerTest.java
+++ src/test/java/com/unclew/web/HomeControllerTest.java
@@ -1,8 +1,10 @@
 package com.unclew.web;
 
 import org.junit.Test;
-
-import static org.junit.Assert.assertEquals;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
 
 /**
  * Created by wuyingqiang
@@ -12,6 +14,7 @@ public class HomeControllerTest {
     @Test
     public void testHomePage() throws Exception {
         HomeController controller = new HomeController();
-        assertEquals("home", controller.home());
+        MockMvc mockMvc = standaloneSetup(controller).build();
+        mockMvc.perform(get("/")).andExpect(view().name("home")); // expect view
     }
 }
